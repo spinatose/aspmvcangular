143d957cbe888cd4d1605e3e5693bbdd14783f5a
Scot Pfuntner <spinatose@gmail.com>
2023-10-02 20:29:09 -0500

    baseline project with just index.html navigated to

---
+++ DutchTreat/Program.cs
@@ -0,0 +1,25 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+//if (!app.Environment.IsDevelopment())
+//{
+//    app.UseExceptionHandler("/Error");
+//}
+
+// The order here is important. 
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+//app.UseRouting();
+
+//app.UseAuthorization();
+
+// app.MapRazorPages();
+
+app.Run();
+
